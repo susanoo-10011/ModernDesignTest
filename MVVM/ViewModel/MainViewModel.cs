45a955617940318b841f9e1268efb26feb8f6bc5
susanoo-10011 <cycahosir@gmail.com>
2023-04-11 21:08:55 +0700

    Добавьте файлы проекта.

---
+++ MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using ModernDesign.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernDesign.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+        public DiscoveryViewModel DiscoveryVM { get; set; }
+
+        private object _cuerrentView;
+
+        public object CurrentView
+        {
+            get { return _cuerrentView; }
+            set
+            {
+                _cuerrentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            DiscoveryVM = new DiscoveryViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o => 
+            { 
+                CurrentView = HomeVM;
+            });
+            
+            DiscoveryViewCommand = new RelayCommand(o => 
+            { 
+                CurrentView = DiscoveryVM;
+            });
+
+        }
+
+    }
+}
